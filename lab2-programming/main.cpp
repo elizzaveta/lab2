39e317b52020aafdf106f47c20ae0e532f9110e8
elizzaveta <lizatimcenko5@gmail.com>
2020-03-01 09:59:42 +0200

    Added names_of_budg()

--- lab2-programming/main.cpp
+++ lab2-programming/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <fstream>
 #include <iomanip>
 #include <string>
@@ -8,4 +9,15 @@ using namespace std;
 int main() {
 
 
+}
+
+//сделать массив имен бюджетников
+vector<string> names_of_budg(string* all_names, string* if_budg, int n) {
+	vector<string> names;
+	for (int i = 0; i < n; i++) {
+		if (if_budg[i] == "TRUE") {
+			names.push_back(all_names[i]);
+		}
+	}
+	return names;
 }
